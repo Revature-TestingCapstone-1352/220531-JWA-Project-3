d838e01c1230e874c04f917ba701591bbf0c5bb3
Josh <joshfriesner4@gmail.com>
2022-07-21 14:10:43 -0600

    pushing up after making all the front-side files and folders

---
+++ src/test/java/com/revature/pages/CartPage.java
@@ -0,0 +1,25 @@
+package com.revature.pages;
+
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.support.PageFactory;
+
+import com.revature.runner.GameRunner;
+
+public class CartPage {
+
+	public GameRunner driver = new GameRunner();;
+	
+// a constructor to create an instance of this Page, and also initialize the web elements on the page.
+//	public CartPage(WebDriver driver) {
+//		this.driver = (GameRunner) driver;
+//		// this next bit of code is working from the PageFactory
+//		PageFactory.initElements(driver, this);
+//	}
+
+	public  void getsomtehing(){
+//		driver.
+//		driver.driver.
+	}
+
+	
+}
