4b3115fdb87397403193dca218259d255e4e35e9
Josh <joshfriesner4@gmail.com>
2022-07-22 08:01:48 -0600

    added pom parts for the cart page and getting to cart page

--- src/test/java/com/revature/pages/CartPage.java
+++ src/test/java/com/revature/pages/CartPage.java
@@ -1,9 +1,12 @@
 package com.revature.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import com.revature.runner.GameRunner;
 
 //import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.support.PageFactory;
@@ -13,25 +16,42 @@ import org.openqa.selenium.support.PageFactory;
 public class CartPage {
 
 	
-	private WebDriver driver;
+//	private WebDriver driver;
+	private GameRunner driver;;
 
 	// a constructor to create an instance of this Page, and also initialize the web elements on the page.
 	public CartPage(WebDriver driver) {
-		this.driver = driver;
+		this.driver = (GameRunner) driver;
 		// this next bit of code is working from the PageFactory
 		PageFactory.initElements(driver, this);
 	}
 	
 	
 	
-	// 
+	// THIS LOCATES THE TOP LEVEL NAVIGATION CART BUTTON
+	@FindBy(xpath = "/html/body/app-root/router-outlet/app-nav-bar/ul/li[3]/a")
+	public WebElement navCartButton;
 	
-	@FindBy(id ="js-link-box-en")
-	public WebElement englishLink;
 	
 	
-	@FindBy(xpath = "//*[@id=\"js-link-box-it\"]")
-	public WebElement germanLink;
+	// THIS LOCATES THE TOP LEVEL NAVIGATION CART BUTTON
+	@FindBy(xpath = "/html/body/app-root/app-cart/div[1]/h4")
+	public WebElement shoppingCartEmptyMessage;
+	
+	
+	@FindBy(xpath = "//*[@id=\"games\"]/div/div[2]/div/div[4]/div/button[2]")
+	public WebElement addToCartButton;
+	
+	
+	// THIS CART TABLE WILL ONLY APPEAR IF THERE IS AN ITEM IN THE CART
+	@FindBy(xpath = "/html/body/app-root/app-cart/div/div/div[2]/table/tbody")
+	public WebElement cartTable;
+	
+	
+	// THIS WILL BRING UP ALL THE BUTTONS INSIDE OF THE cartTable
+	public WebElement cartTblRemoveButtons = cartTable.findElement(By.tagName("button"));
+	
+	
 
 	
 }
--- src/test/java/com/revature/runner/GameRunner.java
+++ src/test/java/com/revature/runner/GameRunner.java
@@ -2,16 +2,28 @@ package com.revature.runner;
 
 import java.io.File;
 //import io.cucumber.junit.*;
+import java.time.Duration;
 
-import org.junit.platform.suite.api.Suite;
+import org.junit.Test;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.Test;
+//import org.junit.platform.suite.api.Suite;
 //import org.junit.jupiter.api.AfterAll;
 //import org.junit.jupiter.api.BeforeAll;
 //import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 //import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.revature.pages.CartPage;
+//import com.revature.pages.WikiPage;
+import com.revature.steps.CartPageStepImpl;
 
 //
 import io.cucumber.java.AfterAll;
@@ -19,29 +31,20 @@ import io.cucumber.java.BeforeAll;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+//@Suite
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/chromedriver.exe", glue = "com.revature.steps" )
-@Suite
 public class GameRunner {
 	
 	
 	
 	// we'll need a driver and an instance of our WikiPage POM
 	public static  WebDriver driver;
+	// we'll need a driver and an instance of our WikiPage POM
+	public static CartPage cartPage;
+	public static CartPageStepImpl cartPageSteps;
 
 
-	public  WebDriver getDriver() {
-		
-		File chrome = new File("src/test/resources/chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver",chrome.getAbsolutePath());
-//		
-		driver = new ChromeDriver();
-		return driver;
-	}
-
-
-
-	
 	@BeforeAll //@BeforeClass is JUnit 4
 	public static void setup() {
 		File chrome = new File("src/test/resources/chromedriver.exe");
@@ -50,8 +53,17 @@ public class GameRunner {
 		driver = new ChromeDriver();
 		
 		//ADD YOUR WEB PAGES HERE
+		cartPage = new CartPage(driver);
 
 	}
+	
+	
+	
+	@Test
+	public void canClickCartButton() throws InterruptedException {
+		cartPageSteps.randoTesto();
+	}
+	
 	
 	@AfterAll
 	public static void teardown() {
