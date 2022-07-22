4e309d5521d27c1e2496de12db63f9d07ec44bf0
Jude Morera <jude960@revature.net>
2022-07-22 16:03:23 -0400

    slight cleanup

--- src/test/java/com/revature/steps/SearchStepImpl.java
+++ src/test/java/com/revature/steps/SearchStepImpl.java
@@ -19,39 +19,38 @@ import com.revature.runner.GameRunner;
 import io.cucumber.java.en.*;
 
 public class SearchStepImpl {
-	
+
 	public static WebDriver driver = GameRunner.driver;
 	public static HomePage homePage = GameRunner.homePage;
-	
+
 	@Given("The user is on the homepage")
 	public void the_user_is_on_the_homepage() {
-		
-		
-	    driver.get("http://localhost:4200/");
-	    new WebDriverWait(driver, Duration.ofSeconds(3));
+
+
+		driver.get("http://localhost:4200/");
+		new WebDriverWait(driver, Duration.ofSeconds(3));
 
 	}
 
 	@When("I search for {string}")
 	public void i_search_for(String string) {
-	HomePage homePage = new HomePage(driver);
-	   string = "Dawn of";
+		HomePage homePage = new HomePage(driver);
+		string = "Dawn of";
 
-	   driver.findElement(homePage.getSearchBar()).sendKeys(string);
-	   
-	   
-//	   new WebDriverWait(driver, Duration.ofSeconds(3));
+		driver.findElement(homePage.getSearchBar()).sendKeys(string);
+
+
+		//	   new WebDriverWait(driver, Duration.ofSeconds(3));
 	}
 
 	@Then("The first game should be {string}")
 	public void the_first_game_should_be(String string) {
-				.findElements(By.xpath("./child::*"));
 		String item = driver.findElement(By
 				.xpath("/html/body/app-root/app-store/div/div/div[2]/div/div[1]/div/div[2]")).getText(); 
-		
-	   string = "Warhammer 40,000: Dawn of War II: Retribution";
-	   
-	   assertEquals(string,item);
+
+		string = "Warhammer 40,000: Dawn of War II: Retribution";
+
+		assertEquals(string,item);
 	}
-	
+
 }
