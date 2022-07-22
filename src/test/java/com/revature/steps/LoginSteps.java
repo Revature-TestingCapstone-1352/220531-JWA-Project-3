adbf51720329295590fd3bc89ad231d1a29998ed
wlcross <william589@revature.net>
2022-07-22 16:03:29 -0400

    Feature File and empty step implementations

---
+++ src/test/java/com/revature/steps/LoginSteps.java
@@ -0,0 +1,139 @@
+package com.revature.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+	
+	
+	@Given("the user is on the login page")
+	public void the_user_is_on_the_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user enters valid credentials")
+	public void the_user_enters_valid_credentials() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user clicks the log in button")
+	public void the_user_clicks_the_log_in_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user is redirected to the store page")
+	public void the_user_is_redirected_to_the_store_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user is logged in")
+	public void the_user_is_logged_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user enters invalid credentials")
+	public void the_user_enters_invalid_credentials() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user is redirected to the registration page")
+	public void the_user_is_redirected_to_the_registration_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user is not logged in")
+	public void the_user_is_not_logged_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("a user is logged in")
+	public void a_user_is_logged_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("a user has added a game to their cart")
+	public void a_user_has_added_a_game_to_their_cart() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("a user is on the cart page")
+	public void a_user_goes_to_the_cart_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("a user clicks the logout button")
+	public void a_user_clicks_the_logout_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("a user goes to the cart page")
+	public void a_user_goes_to_the_cart_page1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the cart should be empty")
+	public void the_cart_should_be_empty() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("a user has added a game to their wishlist")
+	public void a_user_has_added_a_game_to_their_wishlist() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("a user is on the wishlist page")
+	public void a_user_goes_to_the_wishlist_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("a user goes to the wishlist page")
+	public void a_user_goes_to_the_wishlist_page1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the wishlist should be empty")
+	public void the_wishlist_should_be_empty() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("a user is on the user information page")
+	public void a_user_goes_to_the_user_information_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("a user goes to the user information page")
+	public void a_user_goes_to_the_user_information_page1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user information page should be empty")
+	public void the_user_information_page_should_be_empty() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	
+	
+}
