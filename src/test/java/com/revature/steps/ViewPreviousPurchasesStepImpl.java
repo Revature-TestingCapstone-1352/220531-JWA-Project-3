55840717d812436470cd368ba302761d6d1e7092
Evdude33 <62618387+emllewellyn@users.noreply.github.com>
2022-07-22 18:11:58 -0400

    Setup files for view previous purchase feature

---
+++ src/test/java/com/revature/steps/ViewPreviousPurchasesStepImpl.java
@@ -0,0 +1,33 @@
+package com.revature.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ViewPreviousPurchasesStepImpl {
+
+	
+	@Given("a User is logged in and has made a previous purchase")
+	public void a_user_is_logged_in_and_has_made_a_previous_purchase() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("they click on the User Information button")
+	public void they_click_on_the_user_information_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("then they click on the View Previous Orders button")
+	public void then_they_click_on_the_view_previous_orders_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("they can see previous purchases they have made")
+	public void they_can_see_previous_purchases_they_have_made() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
