b3d0ae4276877ad67b6a2fc2eeada31ca447ed2a
joshuacoombs <h0wlpenwyrm@gmail.com>
2022-07-22 10:44:39 -0700

    adding changes to CheckoutPage, CheckoutSteps, HomePage, and ProductDisplaySteps

--- src/test/java/com/revature/pages/HomePage.java
+++ src/test/java/com/revature/pages/HomePage.java
@@ -24,23 +24,54 @@ public class HomePage {
 	private By uNameField = By.xpath("/html/body/app-root/app-login/div/div/div/div/div/div[1]/input");
 	private By pKeyField = By.xpath("/html/body/app-root/app-login/div/div/div/div/div/div[2]/input");
 	private By loginButton = By.xpath("/html/body/app-root/app-login/div/div/div/div/div/div[3]/button");
-		
-	public void loginForHomePage(String username, String password) {
-		driver.findElement(loginLink).click();
-		driver.findElement(uNameField).sendKeys(username);
-		driver.findElement(pKeyField).sendKeys(password);
-		driver.findElement(loginButton).click();
-	}
 	// ---------------------------------------------------------------------
-	private WebElement gamesDisplayed = driver.findElement(By.xpath("/html/body/app-root/app-store/div/div/div[2]/div"));
+	private By gamesDisplayed = By.xpath("/html/body/app-root/app-store/div/div/div[2]/div");
+
+	public By getLoginLink() {
+		return loginLink;
+	}
+	public By getuNameField() {
+		return uNameField;
+	}
+	public By getpKeyField() {
+		return pKeyField;
+	}
+	public By getLoginButton() {
+		return loginButton;
+	}
+	public By getGamesDisplayed() {
+		return gamesDisplayed;
+	}
+	public void setLoginLink(By loginLink) {
+		this.loginLink = loginLink;
+	}
+	public void setuNameField(By uNameField) {
+		this.uNameField = uNameField;
+	}
+	public void setpKeyField(By pKeyField) {
+		this.pKeyField = pKeyField;
+	}
+	public void setLoginButton(By loginButton) {
+		this.loginButton = loginButton;
+	}
+	public void setGamesDisplayed(By gamesDisplayed) {
+		this.gamesDisplayed = gamesDisplayed;
+	}
 	
+	
+	//private WebElement gamesDisplayed = driver.findElement(By.xpath("/html/body/app-root/app-store/div/div/div[2]/div"));
+	
+	/*
 	public boolean areDisplayed() {
 		return gamesDisplayed.isDisplayed();
 	}
+	*/
 	
+	/*
 	private List<WebElement> twentyFour = gamesDisplayed.findElements(By.xpath("./child::*"));
 	
 	public int areThereTwentyFour() {
 		return twentyFour.size();
 	}
+	*/
 }
