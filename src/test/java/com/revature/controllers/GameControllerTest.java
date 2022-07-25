5db68c605b847e5d4814970078c1cb13a1722b3b
kevinprovost <kevin962@revature.net>
2022-07-25 16:01:31 -0400

    gamecontroller test added, coverage at 100%. gameservice test fix

--- src/test/java/com/revature/controllers/GameControllerTest.java
+++ src/test/java/com/revature/controllers/GameControllerTest.java
@@ -1,4 +1,45 @@
 package com.revature.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.revature.models.Game;
+import com.revature.services.GameService;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = GameController.class)
 public class GameControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private GameService gameService;
+
+	@Test
+	public void testGetAllGames() throws Exception {
+		List<Game> mockGameList = new ArrayList<Game>();
+		mockGameList.add(new Game());
+
+		Mockito.when(gameService.getAllGames()).thenReturn(mockGameList);
+		RequestBuilder rb = MockMvcRequestBuilders.get("/game").accept(MediaType.APPLICATION_JSON);
+		MvcResult result = mockMvc.perform(rb).andReturn();
+		String expected = "[{\"gameID\":0,\"storeID\":0.0,\"name\":null,\"steamAppID\":0.0,\"salePrice\":0.0,\"retailPrice\":0.0,\"steamRatingText\":null,\"steamRatingPercent\":0.0,\"steamRatingCount\":0.0,\"metacriticScore\":0.0,\"metacriticLink\":null,\"releaseDate\":null,\"publisher\":null,\"thumb\":null}]";
+		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+	};
+
 }
--- src/test/java/com/revature/services/GameServiceTest.java
+++ src/test/java/com/revature/services/GameServiceTest.java
@@ -70,7 +70,7 @@ public class GameServiceTest {
 
 		when(mockGameDao.findAll()).thenReturn(mockGameList);
 
-		assertEquals(mockGameList.size(), gameService.getAllGames().size());
+		assertEquals(mockGameList, gameService.getAllGames());
 	}
 
 	@Test
