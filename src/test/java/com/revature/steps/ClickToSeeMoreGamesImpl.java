9b1b582ad00a22c17328d6ec75b91e74f5130b3e
Tyler Blackmon <tsb34@students.uwf.edu>
2022-07-25 09:30:29 -0500

    Both dark mode and game page features complete

--- src/test/java/com/revature/steps/ClickToSeeMoreGamesImpl.java
+++ src/test/java/com/revature/steps/ClickToSeeMoreGamesImpl.java
@@ -26,8 +26,39 @@ public class ClickToSeeMoreGamesImpl {
         homePage.clickNextPageButton();
     }
 
-    @Then("the page will display more games")
-    public void the_page_will_display_more_games() {
+    @Then("the page will display next game page")
+    public void the_page_will_display_next_game_page() {
         assertEquals(homePage.elementTextAfterClickingNextPage(), "Demigod");
+    }
+
+    @Given("that a user is on the HomePage and not on first game page")
+    public void that_a_user_is_on_the_HomePage_and_not_on_first_game_page() {
+        driver.get("http://localhost:4200");
+        homePage.clickNextPageButton();
+    }
+
+    @When("the User clicks the previousPageButton")
+    public void the_user_clicks_the_previousPageButton() {
+        homePage.clickPreviousPageButton();
+    }
+
+    @Then("the page will display previous game page")
+    public void the_page_will_display_previous_game_page() {
+        assertEquals(homePage.elementTextAfterClickingPreviousButton(), "Evochron Mercenary");
+    }
+
+    @Given("that a user is on the HomePage viewing first game page")
+    public void that_a_user_is_on_the_HomePage_viewing_first_game_page() {
+        driver.get("http://localhost:4200");
+    }
+
+    @When("the User clicks numberedPageButton")
+    public void the_user_clicks_the_numberedPageButton() {
+        homePage.clickNumberedPage();
+    }
+
+    @Then("the page will display specified game page")
+    public void the_page_will_display_specified_game_page() {
+        assertEquals(homePage.elementTextAfterClickingPageNumber(), "PT Boats: South Gambit");
     }
 }
--- src/test/resources/features/ClickToSeeMoreGames.feature
+++
@@ -1,7 +0,0 @@
-Feature: Click to see more games
-
-  Scenario: Click arrow to go to next game page
-    Given that a user is on the HomePage viewing games
-    When the User clicks the nextPageButton
-    Then the page will display more games
-
