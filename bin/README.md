f226af71e97b6bc591ac7b3a578eb36696a7a5c2
Tyler Blackmon <tsb34@students.uwf.edu>
2022-07-25 16:26:07 -0500

    gitignored

--- bin/README.md
+++
@@ -1,2 +0,0 @@
-# 220531-JWA-Project-3
-220531 JWA project 3
--- bin/src/app/models/orderhistory.ts
+++
@@ -1,6 +0,0 @@
-import { Cart } from "./cart";
-import { Game } from "./game";
-
-export class Orderhistory {
-    constructor(public orderId:number, public orderDate:Date, public games:Cart[], public totalPrice:number){}
-}
