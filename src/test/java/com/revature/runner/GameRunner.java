5ebe655ee23191fed0eef5fbcb52addd803591ce
Robert Randolph <robprand@gmail.com>
2022-07-26 14:24:28 -0600

    Fixed Runner

--- src/test/java/com/revature/runner/GameRunner.java
+++ src/test/java/com/revature/runner/GameRunner.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import com.revature.pages.CheckoutPage;
 import com.revature.pages.HomePage;
 import com.revature.pages.LoginPage;
+import com.revature.pages.RegisterPage;
 import com.revature.pages.UserInfoPage;
 import com.revature.pages.WishListPage;
 
@@ -24,56 +25,54 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features = "src/test/resources/chromedriver.exe", glue = "com.revature.steps" )
 @Suite
 public class GameRunner {
-	
-	
-	
-	// we'll need a driver and an instance of our WikiPage POM
-	public static  WebDriver driver;
-	public static LoginPage loginPage;
-  public static CheckoutPage checkoutPage;
-	public static WishListPage wishListPage;
-	public static HomePage homePage;
-// Khellil code
-	public static UserInfoPage userInfoPage;
-	// ==========
+    
+    
+    
+    // we'll need a driver and an instance of our WikiPage POM
+    public static  WebDriver driver;
+    public static LoginPage loginPage;
+    public static CheckoutPage checkoutPage;
+    public static WishListPage wishListPage;
+    public static HomePage homePage;
+    public static UserInfoPage userInfoPage;
 
-	public  WebDriver getDriver() 
+    
+    public  WebDriver getDriver() 
   {
-		
-		File chrome = new File("src/test/resources/chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver",chrome.getAbsolutePath());
-		driver = new ChromeDriver();
-		
-		
-		return driver;
-	}
+        
+        File chrome = new File("src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",chrome.getAbsolutePath());
+        driver = new ChromeDriver();
+        
+        
+        return driver;
+    }
+    
 
 
 
-	
-	@BeforeAll //@BeforeClass is JUnit 4
-	public static void setup() {
-		File chrome = new File("src/test/resources/chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver",chrome.getAbsolutePath());
-		 
-		driver = new ChromeDriver();
-		
-		checkoutPage = new CheckoutPage(driver);
-		loginPage = new LoginPage(driver);
-		wishListPage = new WishListPage(driver);
-		homePage = new HomePage(driver);
-		userInfoPage = new UserInfoPage(driver);
-		
-		//ADD YOUR WEB PAGES HERE
-		driver.get("http://localhost:4200");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-	}
-	
-	@AfterAll
-	public static void teardown() 
+    
+    @BeforeAll //@BeforeClass is JUnit 4
+    public static void setup() {
+        File chrome = new File("src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",chrome.getAbsolutePath());
+        
+        driver = new ChromeDriver();
+        
+        checkoutPage = new CheckoutPage(driver);
+        loginPage = new LoginPage(driver);
+        wishListPage = new WishListPage(driver);
+        homePage = new HomePage(driver);
+        userInfoPage = new UserInfoPage(driver);
+        
+        //ADD YOUR WEB PAGES HERE
+    }
+    
+    @AfterAll
+    public static void teardown() 
   {
-		driver.quit();
-	}
-	
-	
+        driver.quit();
+    }
+    
+    
 }
