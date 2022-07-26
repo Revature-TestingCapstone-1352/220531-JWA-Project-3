97eff8af208a243f071a65dbc7373a16da430349
JudeMorera <106624471+JudeMorera@users.noreply.github.com>
2022-07-26 16:12:57 -0400

    Delete bin directory

--- bin/src/app/models/cart.ts
+++
@@ -1,6 +0,0 @@
-import { Game } from "./game";
-import { User } from "./user";
-
-export class Cart {
-    constructor(public cartId:number, public game:Game, public quantity:number, public user?:User){}
-}
